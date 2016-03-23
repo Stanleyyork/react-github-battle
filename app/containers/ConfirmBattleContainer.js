81e2dfb01487efdb155c41be4ef05e96a7dc5887
stanleyyork <stanleyyork@gmail.com>
2016-03-23 15:54:21 -0400

    axios, promises, github api and sloppy DOM rendering

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require ('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
 	contextTypes: {
@@ -10,7 +11,7 @@ var ConfirmBattleContainer = React.createClass({
 		return {
 			isLoading: true,
 			playerInfo: []
-		}
+		};
 	},
 	componentWillMount: function () {
 		console.log("componentWillMount");
@@ -18,7 +19,12 @@ var ConfirmBattleContainer = React.createClass({
 	componentDidMount: function () {
 		console.log("componentDidMount");
 		var query = this.props.location.query;
-		console.log("QUERY", query);
+		githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo]).then(function (players) {
+			this.setState({
+				isLoading: false,
+				playersInfo: [players[0], players[1]]
+			});
+		}.bind(this));
 	},
 	componentWillReceiveProps: function () {
 		console.log("componentWillReceiveProps");
